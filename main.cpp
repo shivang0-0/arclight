c575a89ce1bb74fbac9288453ad04b7b851710c0
21103010 <21103010@pdc.jiit>
2022-11-11 10:07:58 +0530

    added playlist option

--- main.cpp
+++ main.cpp
@@ -224,19 +224,124 @@ public:
         }
     }
 };
+class playlist
+{
+    song *head;
+public:
+    playlist()
+    {
+        head = NULL;
+    }
+    void addSong(details d)
+    {
+        song *s = new song;
+        s->d = d;
+        if(head==NULL)
+        {
+            head = s;
+            s->prev = NULL;
+        }
+        else
+        {
+            song *temp = head;
+            while(temp->next!=NULL)
+                temp = temp->next;
+            temp->next = s;
+            s->prev = temp;
+        }
+        s->next = NULL;
+    }
+    bool delSong(details d)
+    {
+        int flag = 0;
+        song *temp = head;
+        if(d.equality(temp->d))
+            goto skipWhile;
+        while(temp->next!=NULL)
+        {
+            if((d.equality((temp->next)->d)))
+            {
+                flag = 1;
+                break;
+            }
+            else
+                temp = temp->next;
+        }
+skipWhile:
+        if(flag)
+        {
+            song* tbd = temp->next;
+            temp->next = tbd->next;
+            delete tbd;
+            return true;
+        }
+        else
+            return false;
+    }
+    bool printSongs()
+    {
+        int i = 1;
+        song* temp = head;
+        while(temp!=NULL)
+        {
+            cout<<"Song number "<<i<<" : "<<endl;
+            (temp->d).printDetails();
+            cout<<endl;
+            temp = temp->next;
+            i++;
+        }
+        return true;
+    }
+    bool searchSong(details d)
+    {
+        song* temp = head;
+        while(temp!=NULL)
+        {
+            if(temp->d.returnFileName() == d.returnFileName())
+                return true;
+            else
+                temp = temp->next;
 
+        }
+        return false;
+    }
+    void playPlaylist()
+    {
+        song* temp = head;
+        while(temp!=NULL)
+        {
+            playSong(temp->d);
+            temp = temp->next;
+        }
+    }
+    ~playlist()
+    {
+        song* temp;
+        while(head!=NULL)
+        {
+            temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+};
 int main()
 {
     details d[10];
     songQueue q;
     int ch,w=0,opt;
+    int i=0,x,y,z;
+    playlist p[10];
     writeSongs(d);
+
     while(true)
     {
         cout<<"1: Play Single Song"<<endl;
         cout<<"2: Add songs to Queue"<<endl;
         cout<<"3: Play queue"<<endl;
-        cout<<"4: Exit"<<endl;
+        cout<<"4: Create Playlist"<<endl;
+        cout<<"5: Play existing Playlist"<<endl;
+        cout<<"6: Exit"<<endl;
         cout<<"Enter choice: ";
         cin>>ch;
         switch(ch)
@@ -302,6 +407,51 @@ replay:
             break;
         }
         case 4:
+        {
+            if(i<=9)
+            {
+                opt = 1;
+                cout<<"Play list name: Play list-"<<i+1<<"\n";
+                while(opt != 0)
+                {
+                    for(x=0; x<5; x++)
+                    {
+                        cout<<"Song number: "<<x+1<<endl;
+                        d[x].printDetails();
+                        cout<<"\n";
+                    }
+                    cout<<"Enter song number to add to play list: ";
+                    cin>>y;
+                    p[i].addSong(d[y-1]);
+                    cout<<"Do you want to add another song(1/0): ";
+                    cin>>opt;
+                }
+                i++;
+            }
+            else
+                cout<<"Maximum play list created!";
+            break;
+        }
+        case 5:
+        {
+            if(i==0)
+            {
+                cout<<"No play list to play yet!\n";
+                break;
+            }
+            cout<<"Enter the play list number to play: ";
+            cin>>z;
+            if(z>10 || z>i)
+            {
+                cout<<"Not a valid play list";
+                break;
+            }
+            cout<<"\n\n";
+            p[z-1].printSongs();
+            p[z-1].playPlaylist();
+            break;
+        }
+        case 6:
         {
             cout<<"THANK YOU "<<(char)1<<endl;
             exit(0);
